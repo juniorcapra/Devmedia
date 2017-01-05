1494e32a2092d1fb3f99255e43df924d435c0a9c
Junior Capra <acaprajr@gmail.com>
2017-01-05 21:35:08 -0200

    novos testes

--- src/br/edu/devmedia/testes/TestesCollections.java
+++ src/br/edu/devmedia/testes/TestesCollections.java
@@ -1,15 +1,22 @@
 package br.edu.devmedia.testes;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.ImmutableSet;
 
+import gnu.trove.list.linked.TLinkedList;
 import gnu.trove.map.TMap;
 import gnu.trove.map.hash.THashMap;
+import gnu.trove.map.hash.TIntIntHashMap;
+import gnu.trove.map.hash.TObjectIntHashMap;
 import gnu.trove.set.hash.THashSet;
 
 public class TestesCollections {
@@ -21,8 +28,9 @@ public class TestesCollections {
 	}
 	
 	public static void testaPerformanceGuava() {
-		Builder<String, Pessoa> mapaGuava = ImmutableMap.<String, Pessoa>builder();
-//		THashSet<Pessoa> hasSet = new THashSet<Pessoa>();
+		Builder<String, Integer> mapaGuava = ImmutableMap.<String, Integer>builder();
+		com.google.common.collect.ImmutableSet.Builder<Pessoa> hashSet = ImmutableSet.<Pessoa>builder();
+		com.google.common.collect.ImmutableList.Builder<Pessoa> lista = ImmutableList.<Pessoa>builder();
 		long tempo = Calendar.getInstance().getTimeInMillis();
 		
 		for (int i = 0; i < 100000 * 10; i++) {
@@ -31,8 +39,9 @@ public class TestesCollections {
 			
 			Pessoa pessoa = new Pessoa(i, "Diogo Castro" + i, 56.59 + i, endereco);
 			
-			mapaGuava.put("Diogo" + i, pessoa);
-//			hasSet.add(pessoa);
+			mapaGuava.put("Diogo" + i, i);
+//			hashSet.add(pessoa);
+			lista.add(pessoa);
 		}
 		System.out.println(Calendar.getInstance().getTimeInMillis() - tempo);
 	}
@@ -40,6 +49,9 @@ public class TestesCollections {
 	public static void testaPerformanceTrove() {
 		TMap<String, Pessoa> mapaTrove = new THashMap<String, Pessoa>();
 		THashSet<Pessoa> hasSet = new THashSet<Pessoa>();
+		
+		TIntIntHashMap mapInt = new TIntIntHashMap();
+		
 		long tempo = Calendar.getInstance().getTimeInMillis();
 
 		for (int i = 0; i < 100000 * 10; i++) {
@@ -48,15 +60,16 @@ public class TestesCollections {
 			
 			Pessoa pessoa = new Pessoa(i, "Diogo Castro" + i, 56.59 + i, endereco);
 			
-			mapaTrove.put("Diogo" + i, pessoa);
-//			hasSet.add(pessoa);
+//			mapaTrove.put("Diogo" + i, pessoa);
+			mapInt.put(i, i);
 		}
 		System.out.println(Calendar.getInstance().getTimeInMillis() - tempo);
 	}
 	
 	public static void testaPerformanceJDK() {
-		Map<String, Pessoa> mapaJDK = new HashMap<String, Pessoa>();
+		Map<String, Integer> mapaJDK = new HashMap<String, Integer>();
 		HashSet<Pessoa> hashSet = new HashSet<Pessoa>();
+		List<Pessoa> lista = new ArrayList<Pessoa>();
 		long tempo = Calendar.getInstance().getTimeInMillis();
 		
 		for (int i = 0; i < 100000 * 10; i++) {
@@ -65,8 +78,9 @@ public class TestesCollections {
 			
 			Pessoa pessoa = new Pessoa(i, "Diogo Castro" + i, 56.59 + i, endereco);
 			
-			mapaJDK.put("Diogo" + i, pessoa);
+			mapaJDK.put("Diogo" + i, i);
 //			hashSet.add(pessoa);
+//			lista.add(pessoa);
 		}
 		System.out.println(Calendar.getInstance().getTimeInMillis() - tempo);
 	}
